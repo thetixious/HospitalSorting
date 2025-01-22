d366470d9ccb6a4f282b4a521a58e92d1d0979b9
Arkadiy <ri.arkadiy@gmail.com>
2025-01-22 15:36:51 +0300

    init commit

---
+++ src/main/java/org/tix/hospital_sorting/HospitalSortingApplication.java
@@ -0,0 +1,13 @@
+package org.tix.hospital_sorting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalSortingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HospitalSortingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/tix/hospital_sorting/HospitalSortingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tix.hospital_sorting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalSortingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
